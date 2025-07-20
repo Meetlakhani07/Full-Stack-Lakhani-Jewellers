620f0787516bed50f0910d2f57809e85a6c3bfc0
Meet Lakhani <147502562+Meetlakhani07@users.noreply.github.com>
2025-07-20 13:49:48 +0100

    done

--- Lakhani/frontend/index.js
+++ Lakhani/frontend/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import axios from 'axios';
 
 // Set the base URL for Axios from your environment variable
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'https://lakhani-jewellers.onrender.com/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
