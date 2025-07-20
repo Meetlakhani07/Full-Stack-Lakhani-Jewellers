1e74a1115721a1fa718737b2cb98b3aee4368a7d
Meet Lakhani <147502562+Meetlakhani07@users.noreply.github.com>
2025-07-20 13:17:31 +0100

    App

--- Lakhani/frontend/src/App.jsx
+++ Lakhani/frontend/src/App.jsx
@@ -15,113 +15,114 @@ import OrderPage from "./pages/OrderPage";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 import MyOrders from "./pages/MyOrders";
 import AboutUsPage from "./pages/AboutUsPage";
+import WishList from "./pages/Wishlist";
 
-// Admin pages
+// Admin pages (use consistent folder casing: "Admin")
 import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
-import AdminOrders from "./pages/admin/AdminOrders.jsx";
+import AdminOrders from "./pages/Admin/AdminOrders.jsx";
 import AdminOrderDetail from "./pages/Admin/AdminOrderDetail.jsx";
-import AdminProducts from "./pages/admin/AdminProducts.jsx";
+import AdminProducts from "./pages/Admin/AdminProducts.jsx";
 import AdminProductEdit from "./pages/Admin/AdminProductEdit.jsx";
-import AdminCategories from "./pages/admin/AdminCategories.jsx";
+import AdminCategories from "./pages/Admin/AdminCategories.jsx";
 import AdminShopInfo from "./pages/Admin/AdminShopInfo.jsx";
+import AdminProductAdd from "./pages/Admin/AdminProductAdd.jsx";
 
 // Route protection
 import AdminRoute from "./context/AdminRoutes";
-import WishList from "./pages/Wishlist";
-import AdminProductAdd from "./pages/Admin/AdminProductAdd";
 
 function App() {
-  return (
-    <div className="home_black_version">
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/product/:id" element={<ProductDetailPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/wishlist" element={<WishList />} />
-        <Route path="/MyOrders" element={<MyOrders />} />
-        <Route path="/order/:id" element={<OrderPage />} />
-        <Route path="/about" element={<AboutUsPage />} />
-        <Route
-          path="/order-confirmation/:id"
-          element={<OrderConfirmationPage />}
-        />
+return (
+<div className="home_black_version">
+<Header />
+<Routes>
+{/* Public Routes */}
+<Route path="/" element={<HomePage />} />
+<Route path="/products" element={<ProductsPage />} />
+<Route path="/product/:id" element={<ProductDetailPage />} />
+<Route path="/cart" element={<CartPage />} />
+<Route path="/checkout" element={<CheckoutPage />} />
+<Route path="/login" element={<LoginPage />} />
+<Route path="/register" element={<RegisterPage />} />
+<Route path="/account" element={<AccountPage />} />
+<Route path="/wishlist" element={<WishList />} />
+<Route path="/MyOrders" element={<MyOrders />} />
+<Route path="/order/:id" element={<OrderPage />} />
+<Route path="/about" element={<AboutUsPage />} />
+<Route
+path="/order-confirmation/:id"
+element={<OrderConfirmationPage />}
+/>
 
-        {/* Admin Routes */}
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <AdminDashboard />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/orders"
-          element={
-            <AdminRoute>
-              <AdminOrders />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/orders/:id"
-          element={
-            <AdminRoute>
-              <AdminOrderDetail />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/products"
-          element={
-            <AdminRoute>
-              <AdminProducts />
-            </AdminRoute>
-          }
-        />
-
-        <Route
-          path="/admin/products/new"
-          element={
-            <AdminRoute>
-              <AdminProductAdd />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/products/:id"
-          element={
-            <AdminRoute>
-              <AdminProductEdit />
-            </AdminRoute>
-          }
-        />
-
-        <Route
-          path="/admin/categories"
-          element={
-            <AdminRoute>
-              <AdminCategories />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/shop-info"
-          element={
-            <AdminRoute>
-              <AdminShopInfo />
-            </AdminRoute>
-          }
-        />
-      </Routes>
-    </div>
-  );
+php-template
+Copy
+Edit
+    {/* Admin Routes */}
+    <Route
+      path="/admin"
+      element={
+        <AdminRoute>
+          <AdminDashboard />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/orders"
+      element={
+        <AdminRoute>
+          <AdminOrders />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/orders/:id"
+      element={
+        <AdminRoute>
+          <AdminOrderDetail />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/products"
+      element={
+        <AdminRoute>
+          <AdminProducts />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/products/new"
+      element={
+        <AdminRoute>
+          <AdminProductAdd />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/products/:id"
+      element={
+        <AdminRoute>
+          <AdminProductEdit />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/categories"
+      element={
+        <AdminRoute>
+          <AdminCategories />
+        </AdminRoute>
+      }
+    />
+    <Route
+      path="/admin/shop-info"
+      element={
+        <AdminRoute>
+          <AdminShopInfo />
+        </AdminRoute>
+      }
+    />
+  </Routes>
+  <Footer />
+</div>
+);
 }
-
-export default App;
